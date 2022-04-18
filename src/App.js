d641c7922aa367d31f3ae1ef14f45184b201cab9
yusup1802 <yusuperlangga18@gmail.com>
2022-04-19 05:14:51 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className=" texttext-3xl text-blue-500 font-bold underline">
+        Hello world!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
